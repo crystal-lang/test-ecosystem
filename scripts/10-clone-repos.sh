4a0bfc75d2b82572d92176179437da74e6adebd7
Brian J. Cardiff <bcardiff@gmail.com>
2021-02-18 18:46:34 -0300

    Update overrides

--- scripts/10-clone-repos.sh
+++ scripts/10-clone-repos.sh
@@ -72,6 +72,7 @@ gh_clone TechMagister/liquid.cr
 
 override_shard dexter luckyframework/dexter
 override_shard exception_page bcardiff/exception_page fix-deprecation
+override_shard openssl_ext bcardiff/openssl_ext avoid-return
 
 cat $REPOS_DIR/shard.override.yml
 
