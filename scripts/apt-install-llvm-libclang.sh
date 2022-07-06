99ebd69ae32ea0ff3fc0f6834baeccdf52c3bf0e
Beta Ziliani <beta@manas.tech>
2022-07-06 12:09:17 -0300

    upgrading to llvm 13

--- scripts/apt-install-llvm-libclang.sh
+++ scripts/apt-install-llvm-libclang.sh
@@ -3,7 +3,7 @@
 # llvm libclang
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
-./llvm.sh 10
-apt-get install -y libclang-10-dev
-ln -sf /usr/bin/ld.lld-10 /usr/bin/ld.lld
-export LLVM_CONFIG=/usr/bin/llvm-config-10
+./llvm.sh 13
+apt-get install -y libclang-13-dev
+ln -sf /usr/bin/ld.lld-13 /usr/bin/ld.lld
+export LLVM_CONFIG=/usr/bin/llvm-config-13
