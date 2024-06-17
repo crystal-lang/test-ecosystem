f351d96f1ed88edc051c434978205262597e3b3b
Johannes Müller <straightshoota@gmail.com>
2024-06-17 17:19:58 +0200

    git: `core.autocrlf=false`

--- test/helper/common.bash
+++ test/helper/common.bash
@@ -21,7 +21,7 @@ function git_checkout() {
     git pull origin HEAD
     git submodule update --init
   else
-    git clone --recursive "$URL" "$TARGET"
+    git -c core.autocrlf=false clone --recursive "$URL" "$TARGET"
     cd "$TARGET" || exit 1
   fi
 }
