38028741d180170610f6731352695f504d3c627e
Matías García Isaía <mgarcia@manas.tech>
2025-09-29 20:23:45 -0300

    Install with sudo on Linux

--- setup/ubuntu-24.04.bash
+++ setup/ubuntu-24.04.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && \
-  apt-get install -y libmagic-dev && \
-  apt-get clean && rm -rf /var/lib/apt/lists/*
+sudo apt-get update && \
+  sudo apt-get install -y libmagic-dev && \
+  sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
 
